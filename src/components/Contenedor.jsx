fab1385fef4873fe58505f3296eda4be60e2e9de
Christian Cámara <chcamaraguillen@gmail.com>
2025-11-14 16:39:59 +0100

    estructura de children y añadido de outlet

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,4 +1,6 @@
-function Contenedor({ children }) {
+import { Outlet } from "react-router-dom";
+
+function Contenedor( { children, titulo } ) {
   return (
     <main
       id="main-content"
@@ -6,10 +8,13 @@ function Contenedor({ children }) {
       tabIndex="-1"
       className="min-h-screen bg-linear-to-br from-gray-100 to-gray-200 flex flex-col items-center justify-center p-8"
     >
-      <section aria-labelledby="main-section-title" className="w-full max-w7xl text-center">
-        <h1 id="main-section-title">Intérpretes de películas destacadas</h1>
-        {children}
+      <section aria-labelledby="main-section-title" className="w-full max-w-7xl text-center">
+        {titulo && <h1 id="main-section-title">{titulo}</h1>}
       </section>
+
+      {children}
+
+      <Outlet />
     </main>
   );
 }
