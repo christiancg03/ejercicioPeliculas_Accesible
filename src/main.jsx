932a9e25e444a40b0ab57b7879326616e52915f3
Christian Cámara <chcamaraguillen@gmail.com>
2025-11-14 16:38:58 +0100

    estructura de main

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,9 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // <StrictMode>
-  //   <a
-  //     href="#main-content"
-  //     className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-700 text-white px-4 py-2 rounded z-50"
-  //   >
-  //     Saltar al contenido principal
-  //   </a>
-
-  // </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
